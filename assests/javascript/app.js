4500a200c8fe5b858fa723959a5c6c554b35e74a
Chris Offiong <chrisoffiong@gmail.com>
2018-12-19 02:46:22 -0500

    slight code

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -1,39 +1,38 @@
 var questions = [
     {
     question: "Which of the following is in Portland, Oregon?",
-    answer: "Pittock Mansion",
-    choices: ["1. Statue of Liberty", "2. Ponce City Market", "3. Cloud Gate", "4. Pittock Mansion"]
+    answer: 3,
+    choices: ["Statue of Liberty", "Ponce City Market", "Cloud Gate", "Pittock Mansion"]
     },
     {
     question: "What is the third digit of pi?",
-    answer: "4",
-    choices:["1. 8","2. 3", "3. 4", "4.Mmmmm.."]
+    answer: 3,
+    choices:["8","3", "4", "Mmmmm.."]
     },
     { 
     question: "When was the last leap year?",
-    answer: "2016",
+    answer: 1,
     choices: ["1918", "2016", "1723", "2008"]
     },
     {
     question: "What is commonly refered to as a 'perfect spiral'?",
-    answer: "Fibonaccis number",
+    answer: 2,
     choices: ["Tom Brady's throw", "Fibbonaccis number", "Doughnuts", "Stratus Clouds"]
     },
     { 
     question: "When was the start of the Autumn Equinox?",
-    answer: "September 23rd",
-    choices: ["September 23rd", "December 9th", "Feburary 29th", "4. I don't know this one either"]
+    answer: 0,
+    choices: ["September 23rd", "December 9th", "Feburary 29th", "I don't know this one either"]
     },
     { 
     question: "What is the third element in the periodic table?",
-    answer: "Lithium",
-    choices: ["1. Hydrogen", "2. Helium", "3. Xanandium", "4. Lithium"]
+    answer: 3,
+    choices: ["Hydrogen", "Helium", "Xanandium", "Lithium"]
     }]
 
 
-let nextQuestion = 0;
-
 for (let i = 0; i < questions[0].choices.length; i++) {
+
     // display into the
 }
 
@@ -44,24 +43,35 @@ let twentySecondsTimer = 20
 let randomQuestion = Math.floor(Math.random() * questions.length)
 let currentQuestion = questions[randomQuestion]
 
-function restart() {
+function nextQuestion() {
 
     randomQuestion = Math.floor(Math.random() * questions.length)
+    currentQuestion = questions[randomQuestion]
     twentySecondsTimer = 20
     $('#q1').text(currentQuestion.question);
-    $('#c1').append(currentQuestion.choices[0]) 
-    $('#c2').append(currentQuestion.choices[1]) 
-    $('#c3').append(currentQuestion.choices[2]) 
-    $('#c4').append(currentQuestion.choices[3]) 
+    $('#c1').html(currentQuestion.choices[0]) 
+    $('#c2').html(currentQuestion.choices[1]) 
+    $('#c3').html(currentQuestion.choices[2]) 
+    $('#c4').html(currentQuestion.choices[3])
+    setInterval(decrement, 1000);
 
 }
-$("button").one("click", function() {
+$("button").on("click", function() {
     $("button").hide();
     $('#q1').text(currentQuestion.question);
     $('#c1').append(currentQuestion.choices[0]) 
     $('#c2').append(currentQuestion.choices[1]) 
     $('#c3').append(currentQuestion.choices[2]) 
     $('#c4').append(currentQuestion.choices[3]) 
+    for(let i=0; i<questions.length; i++){
+        for(let j=0; j<questions[i].choices.length; j++){
+            if(questions[i].answer===questions[i].choices[j]){
+                this.style.backgroundColor = 'green';
+            }else{
+                this.style.backgroundColor = 'red';
+            }
+        }
+        }
     let timer = setInterval(function() {
         twentySecondsTimer--;
         $(".timer").html("Time:" + twentySecondsTimer)
@@ -69,23 +79,22 @@ $("button").one("click", function() {
             clearInterval( timer );
             unGuessed++;
             $(".unguessed").html("Unguessed questions:" + unGuessed)
-          restart();
+            alert("Times Up! Correct Answer is" + " " + currentQuestion.choices[currentQuestion.answer])
+          nextQuestion();
         };
     },1000)
- 
-
 })
 
+if((userSelect == rightAnswerIndex) && (answered == true)){
+    answersWrong++;
+    ;
+} else if((userSelect != rightAnswerIndex) && (answered == true)){
+    answersRight++;
+    $('#message').html(messages.incorrect);
+    $('#correctedAnswer').html('The correct answer was: ' + rightAnswerText);
 
-// for(let i=0; i<questions.length; i++){
-// for(let j=0; j<questions[i].choices.length; j++){
-//     if(questions[i].answer===questions[i].choices[j]){
-//         .style.backgroundColor = 'green';
-//     }else{
-//         this.style.backgroundColor = 'red';
-//     }
-// }
-// }
+}
+ 
 /* if(this.choices === this.answer){
     div.style.backgroundColor = "green";
 }else{
